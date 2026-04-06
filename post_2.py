6be965f87a98e34ea3a1f468ee087f629cd3505f
Angus061855 <165797999+Angus061855@users.noreply.github.com>
2026-04-07 03:54:05 +0800

    Update post_2.py

--- post_2.py
+++ post_2.py
@@ -263,10 +263,10 @@ if __name__ == "__main__":
         post_to_threads(post_text)
         update_status(page_id, "已發")
         print("✅ 完成！")
-        send_telegram(f"✅ 帳號B 發文成功！\n主題：{custom_topic}")
+        send_telegram("✅ 帳號B 2 發文成功！")
 
     except Exception as e:
-        error_msg = f"❌ 帳號B 發文失敗！\n主題：{custom_topic}\n錯誤原因：{str(e)}"
+        error_msg = f"❌ 帳號B 2 發文失敗！\n錯誤原因：{str(e)}"
         print(error_msg)
         send_telegram(error_msg)
         raise
